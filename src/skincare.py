ee0b10d0e48fefae656f4df5b3889bf0668722fb
Takhirzhon <tashmatovtahir@gmail.com>
2024-04-28 14:25:29 +0300

    Add inheritance

--- src/skincare.py
+++ src/skincare.py
@@ -3,9 +3,8 @@ from data import Product
 
 class SkinCareProduct(Product):
     def __init__(self, brand, product_type, suitable_for):
-        self.brand = brand
-        self.product_type = product_type
-        self.suitable_for = suitable_for
+        super().__init__(brand, product_type, suitable_for)
+
 
     def __str__(self):
         return f"{self.brand} {self.product_type} (suitable for {', '.join(self.suitable_for)})"
